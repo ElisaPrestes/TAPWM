c5a9b764770b4007efb9ddb2c724eff71716d2e9
ElisaPrestes <102178389+ElisaPrestes@users.noreply.github.com>
2025-04-22 22:38:34 -0300

    Add files via upload

---
+++ Atividade6/script.js
@@ -0,0 +1,7 @@
+
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+let opiniao = prompt("Qual a sua opinião sobre o filme Nosferatu?")
+
+
+alert("Olá " + nome + ", você tem " + idade + " anos e sua opinião sobre o filme Nosferatu é: " + opiniao)
