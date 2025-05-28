e52c663be8aef0b542d186004c3bd33a9b01c479
ElisaPrestes <102178389+ElisaPrestes@users.noreply.github.com>
2025-05-28 18:14:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
+//formas de criar o objeto
 //Object é a matriz. Tuo descende dele
 //tanto faz se for var, let e const
 var aluno1 = new Object();
 aluno1.ra = "0001432512";
 aluno1.nome = "Luis";
 
-
+/*
 //interpolação
 alert(`ra= ${aluno1.ra} nome:${aluno1.nome}`);
 // alert("ra= " + aluno1.ra + "nome: " + aluno1.nome);
 
 var aluno2 = {};
-    aluno2.ra = "00014325120";
-    aluno2["nome"] = "Bruna";
-    aluno2["email"] = "brunaelisa@gmail.com";
+aluno2.ra = "00014325120";
+aluno2["nome"] = "Bruna";
+aluno2["email"] = "brunaelisa@gmail.com";
 
 alert(aluno2["email"]);
 
 
-    //forma literal
-    //NÃO esquecer da sintaxe correta
+//forma literal
+//NÃO esquecer da sintaxe correta
 var aluno3 = {
     ra: "29102910",
     nome: "Isadora",
@@ -31,9 +32,9 @@ alert("ra=" + aluno3.ra + "nome= \n" + aluno3.nome);
 function Aluno(ra, nome) {
     this.ra = ra;
     this.nome = nome;
-    this.MostraDados = function (){
-        return "ra=" + this.ra + " nome="+
-        this.nome;
+    this.MostraDados = function () {
+        return "ra=" + this.ra + " nome=" +
+            this.nome;
     }
 }
 
@@ -52,20 +53,20 @@ alert(aluno5.ra + " " + aluno5.nome);
 
 
 //funcao construtora sem parâmetros
-function Aluno2(){
+function Aluno2() {
     var ra; //encapsulamento escondido
     var nome;
-    this.setRa = function (value){
+    this.setRa = function (value) {
         this.ra = value;
     }
-    this.getRa = function(){
+    this.getRa = function () {
         return this.ra;
     }
 
-    this.setNome = function (value){
+    this.setNome = function (value) {
         this.nome = value;
     }
-    this.getNome = function (){
+    this.getNome = function () {
         return this.nome;
     }
 }
@@ -80,7 +81,7 @@ alert("ra= " + aluno6.getRa() + " nome= " + aluno6.getNome());
 //herança com função construtora usando prototype
 function AlunoADS() {
     var numLab;
-    this.setnumLab = function(value){
+    this.setnumLab = function (value) {
         this.numLab = value;
     }
     this.getnumLab = function () {
@@ -92,17 +93,24 @@ function AlunoADS() {
 //quando existe herança, a propriedade é definida na função/class filha
 
 //herança se faz em:
+
 AlunoADS.prototype = new aluno2();
- var aluno7 = new AlunoADS();
- aluno7.setNome("Andreus");
- aluno7.setRa("123");
- aluno7.setnumLab(5);
- alert(aluno7.getNome() +
-" " + aluno7.getnumLab());
+var aluno7 = new AlunoADS();
+aluno7.setNome("Andreus");
+aluno7.setRa("123");
+aluno7.setnumLab(5);
+alert(`Nome: ${aluno7.getNome()} \nRA: ${aluno7.getRa()} \nNúmero Laboratório ${aluno7.getNumLab()}`);
 
 
-class Aluno1{
-    constructor(){
+AlunoADS.prototype = new Aluno2();
+var aluno7 = new AlunoADS();
+aluno7.setNome("Gabriel Fonseca");
+aluno7.setRa("23445");
+aluno7.setNumLab(5);
+alert(`Nome: ${aluno7.getNome()} \nRA: ${aluno7.getRa()} \nNúmero Laboratório ${aluno7.getNumLab()}`);
+
+class Aluno1 {
+    constructor() {
         this._ra;
         this._nome;
     }
@@ -112,3 +120,100 @@ var objAluno = new Aluno1();
 objAluno.setNome("Bruna");
 objAluno.setRa('0031432512001');
 alert(`nome=${objAluno.getNome()} ra=${objAluno.getRa()}`);
+
+
+class AlunoADS1 extends Aluno1 {
+    constructor() {
+        super()
+        this._numLab;
+    }
+    setNumLab(value) {
+        this._numlab = value;
+    }
+    getNumLab() {
+        return this._numLab;
+    }
+}
+
+var objAlunoADS = new AlunoADS1();
+objAlunoADS.setNome("Isabela");
+objAlunoADS.setRA("123");
+objAlunoADS.setNumLab(6);
+alert(`nome=${objAlunoADS.getNome()}
+    ra=${objAlunoADS.getRa()}
+    numlab=${objAlunoADS.getNumLab()}`)
+*/
+
+//cópia de objetos
+var obj1 = new Object();
+var obj2 = obj1;
+obj1.nome = "Matheus";
+alert(obj2.nome);
+
+
+//metodos e atributos - públicos e privados
+class ContaBancaria {
+    #cpf; //atributo privado
+
+    constructor(nome, saldoInicial) {
+        this.nome = nome;
+        this.saldo = saldoInicial;
+    }
+
+    setCpf(value) {
+        this.cpf = value;
+    }
+    getCpf() {
+        return this.cpf;
+    }
+    getSaldo() {
+        return this.saldo;
+    }
+
+    //metodo publico
+    depositar(valor) {
+        this.saldo += valor;
+        this.#registrarTransacao(valor);
+    }
+    #registrarTransacao(valor) {
+        console.log(`Depósito de R$ ${valor} realizado. \n
+                Saldo R$ ${this.saldo} para cpf
+                ${this.getCpf()} ou ${this.cpf}`);
+    }
+}
+
+const conta = new ContaBancaria("Carlos", 1000);
+conta.setCpf(123);
+conta.depositar(50);
+
+
+const arr = [
+    ['codigo',1],
+    ['nome',"João"],
+    ['idade',23],
+];
+
+//erro de exibição
+const obj = Object.fromEntries(arr);
+alert(obj);
+alert(obj.nome);
+alert(JSON.stringify(obj));
+
+
+//diferença entre os 3 tipos de funções -declarada, expressão de função e função de flecha
+//
+
+const objEmpresa = {
+    empresa: 'ZF do Brasil'
+};
+
+const funcionario = Object.create(objEmpresa, {
+    nome: {
+        value: 'José Carlos'
+    },
+    endereco: {
+        value: 'Av. São Paulo'
+    }
+});
+
+console.log(funcionario.empresa);
