e90197c24a581a7e57498d8e560595f6fd0cd78f
Bruna Almeida <bruna.elisa.prestes@gmail.com>
2025-09-10 18:05:53 -0300

    Adicionando ATV 17

--- Atividade16/exercicios/Exercicio6.js
+++
@@ -1,5 +0,0 @@
-let http = require("http");
-let server = http.createServer(function(req,res){
-    res.end("<html><body>Site da Bru</body></html>");
-});
-server.listen(3000);
---
+++ Atividade16/exercicios/app.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let app = express(); //exec express
+app.get('/', function(req,res){
+    res.send("<html><body>Bem-vinda ao Fatec Sorocaba</body></html>");
+})
+app.get('/historia', function(req,res){
+    res.send("<html><body>História da Fatec Sorocaba</body></html>");
+})
+app.get('/cursos', function(req,res){
+    res.send("<html><body>Cursos da Fatec Sorocaba</body></html>");  
+})
+app.get('/professores', function(req,res){
+    res.send("<html><body>Professores da Fatec Sorocaba</body></html>");  
+})
+
+app.listen(3000, function(){
+    console.log("Servidor com express foi carregado");
+})
